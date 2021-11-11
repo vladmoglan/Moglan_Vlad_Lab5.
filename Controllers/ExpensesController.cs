0a3dae9aa18b4938d9a2139c593ac0389a8a86ff
VLAD-ANDREI MOGLAN <vlad.moglan@stud.ubbcluj.ro>
2021-11-11 19:09:30 +0200

    Add project files.

---
+++ Controllers/ExpensesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moglan_Vlad_Lab5.Models;
+
+namespace Moglan_Vlad_Lab5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        private readonly ExpenseContext _context;
+
+        public ExpensesController(ExpenseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Expenses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ExpenseDTO>>> GetExpense()
+        {
+            return await _context.ExpenseDTO.ToListAsync();
+        }
+
+        // GET: api/Expenses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ExpenseDTO>> GetExpense(int id)
+        {
+            var expense = await _context.ExpenseDTO.FindAsync(id);
+
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return expense;
+        }
+
+        // PUT: api/Expenses/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutExpense(int id, Expense expense)
+        {
+            if (id != expense.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(expense).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExpenseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Expenses
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Expense>> PostExpense(Expense expense)
+        {
+            _context.Expense.Add(expense);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetExpense), new { id = expense.Id }, expense);
+        }
+
+        // DELETE: api/Expenses/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Expense>> DeleteExpense(int id)
+        {
+            var expense = await _context.Expense.FindAsync(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            _context.Expense.Remove(expense);
+            await _context.SaveChangesAsync();
+
+            return expense;
+        }
+
+        private bool ExpenseExists(int id)
+        {
+            return _context.Expense.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Moglan_Vlad_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+
+    }
+}
